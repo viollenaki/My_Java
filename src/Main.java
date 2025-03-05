890c418c9691b63a78069ead1b44ebe08a0bffdf
viollenaki <tanakaakuustik@gmail.com>
2025-03-05 15:18:52 +0600

    update

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,25 @@
-abstract class Employee {
-    String name;
-    int id;
-    double base;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public Employee(String name, int id, double base) {
-        this.name = name;
-        this.id = id;
-        this.base = base;
-    }
-
-    public abstract double calculateSalary();
-
-    public void displayEmployeeInfo() {
-        System.out.println("Employee ID--- " + id);
-        System.out.println("Name--- " + name);
-        System.out.println("Salary--- " + calculateSalary());
-    }
-}
-
-interface Payable {
-    double getPaymentAmount();
-}
-
-class FullTimeEmployee extends Employee implements Payable {
-
-
-    public FullTimeEmployee(String name, int id, double base) {
-        super(name, id, base*1.2);    
-    }
-
-    @Override
-    public double calculateSalary() {
-        return base; 
-    }
-
-    @Override
-    public double getPaymentAmount() {
-        return calculateSalary();
-    }
-}
-
-class ContractEmployee extends Employee implements Payable {
-    private double hourly;
-    private int hours;
-
-    public ContractEmployee(String name, int id, double hourly, int hours) {
-        super(name, id, 0); 
-        this.hourly = hourly;
-        this.hours = hours;
-    }
-
-    @Override
-    public double calculateSalary() {
-        return hourly * hours;
-    }
-
-    @Override
-    public double getPaymentAmount() {
-        return calculateSalary();
-    }
-}
-
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Employee[] employees = new Employee[2];
-        employees[0] = new FullTimeEmployee("Emil", 1, 10000);
-        employees[1] = new ContractEmployee("Akbar", 2, 20, 40);
-        for (Employee e : employees){
-            e.displayEmployeeInfo();
-            System.out.println("payment amount--- " + e.calculateSalary());
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(5);
+        arrayList = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        for (int i=0; i<arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
         }
+
+
+
+
+
+        System.out.println(arrayList);
+        arrayList.remove(0);
+        arrayList.set(0,10);
+        System.out.println(arrayList);
+
+
     }
 }
